c58141ccca23983507c2c285a46d0426fea35358
Abhijeet Kumar <abkumarofficial@gmail.com>
2024-02-09 15:45:22 +0530

    Components created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import CarForm from "./components/CarForm";
+import CarSearch from "./components/CarSearch";
+import CarList from "./components/CarList";
+import CarValue from "./components/CarValue";
+
 function App() {
     return <div>
-        App
+        <CarForm/>
+        <CarSearch/>
+        <CarList/>
+        <CarValue/>
     </div>
 }
 
